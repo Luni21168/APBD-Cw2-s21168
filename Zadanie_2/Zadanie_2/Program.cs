73185d47ca6cbb272a793406e4e96c692efe6686
Zlata Oleksiuk <s21168@pjwstk.edu.pl>
2026-03-26 18:05:02 +0100

    Connect services in Program and run reports

--- Zadanie_2/Zadanie_2/Program.cs
+++ Zadanie_2/Zadanie_2/Program.cs
@@ -1,1 +1,53 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Zadanie_2.Repositories;
+using Zadanie_2.Services;
+using Zadanie_2.UI;
+
+namespace Zadanie_2;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var equipmentRepository = new EquipmentRepository();
+        var userRepository = new UserRepository();
+        var rentalRepository = new RentalRepository();
+
+        var rentalPolicy = new RentalPolicy();
+        var penaltyCalculator = new PenaltyCalculator();
+
+        var rentalService = new RentalService(
+            equipmentRepository,
+            userRepository,
+            rentalRepository,
+            rentalPolicy,
+            penaltyCalculator
+        );
+
+        var reportService = new ReportService(
+            equipmentRepository,
+            userRepository,
+            rentalRepository
+        );
+
+        DemoDataSeeder.Seed(userRepository, equipmentRepository, rentalService);
+
+        Console.WriteLine("ALL EQUIPMENT");
+        reportService.PrintAllEquipment();
+
+        Console.WriteLine();
+        Console.WriteLine("AVAILABLE EQUIPMENT");
+        reportService.PrintAvailableEquipment();
+
+        Console.WriteLine();
+        Console.WriteLine("ACTIVE RENTALS FOR USER 1");
+        reportService.PrintActiveRentalsForUser(1);
+
+        Console.WriteLine();
+        Console.WriteLine("OVERDUE RENTALS");
+        reportService.PrintOverdueRentals(DateTime.Today);
+
+        Console.WriteLine();
+        Console.WriteLine("FINAL REPORT");
+        reportService.PrintSummaryReport();
+    }
+}
--- Zadanie_2/Zadanie_2/obj/Debug/net10.0/Zadanie_2.AssemblyInfo.cs
+++ Zadanie_2/Zadanie_2/obj/Debug/net10.0/Zadanie_2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Zadanie_2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a68a89b24a21a163140a8f465070e21ef4757ad2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8225b6ee7022df3ea505fc37a4e29b38761cb1f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Zadanie_2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Zadanie_2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
