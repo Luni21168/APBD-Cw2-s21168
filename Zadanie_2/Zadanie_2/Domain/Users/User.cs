53d536e641bb48a582a0e675a32843280adb9a08
Zlata Oleksiuk <s21168@pjwstk.edu.pl>
2026-03-26 13:34:22 +0100

    Add base user class

--- Zadanie_2/Zadanie_2/Domain/Users/User.cs
+++ Zadanie_2/Zadanie_2/Domain/Users/User.cs
@@ -1,6 +1,26 @@
+using Zadanie_2.Domain.Enums;
+
 namespace Zadanie_2.Domain.Users;
 
-public class User
+public abstract class User
 {
-    
+    public int Id { get; }
+    public string FirstName { get; }
+    public string LastName { get; }
+    public UserType UserType { get; }
+
+    protected User(int id, string firstName, string lastName, UserType userType)
+    {
+        Id = id;
+        FirstName = firstName;
+        LastName = lastName;
+        UserType = userType;
+    }
+
+    public string FullName => $"{FirstName} {LastName}";
+
+    public override string ToString()
+    {
+        return $"{UserType} | Id: {Id} | {FullName}";
+    }
 }
