3947ffcb398eeefb8e90d7b14f16e87874f49ca2
Zlata Oleksiuk <s21168@pjwstk.edu.pl>
2026-03-26 13:20:05 +0100

    Add base equipment class

--- Zadanie_2/Zadanie_2/Domain/Equipment/Equipment.cs
+++ Zadanie_2/Zadanie_2/Domain/Equipment/Equipment.cs
@@ -1,6 +1,37 @@
+using Zadanie_2.Domain.Enums;
+
 namespace Zadanie_2.Domain.Equipment;
 
-public class Equipment
+public abstract class Equipment
 {
-    
+    public int Id { get; }
+    public string Name { get; }
+    public EquipmentStatus Status { get; private set; }
+
+    protected Equipment(int id, string name)
+    {
+        Id = id;
+        Name = name;
+        Status = EquipmentStatus.Available;
+    }
+
+    public void MarkAsAvailable()
+    {
+        Status = EquipmentStatus.Available;
+    }
+
+    public void MarkAsRented()
+    {
+        Status = EquipmentStatus.Rented;
+    }
+
+    public void MarkAsUnavailable()
+    {
+        Status = EquipmentStatus.Unavailable;
+    }
+
+    public override string ToString()
+    {
+        return $"{GetType().Name} | Id: {Id} | Name: {Name} | Status: {Status}";
+    }
 }
