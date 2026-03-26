81233cf7d437f5143dac6d534087faa5c689aac6
Zlata Oleksiuk <s21168@pjwstk.edu.pl>
2026-03-26 13:40:05 +0100

    Add student and employee classes

--- Zadanie_2/Zadanie_2/Domain/Users/Employee.cs
+++ Zadanie_2/Zadanie_2/Domain/Users/Employee.cs
@@ -1,6 +1,21 @@
+using Zadanie_2.Domain.Enums;
+
 namespace Zadanie_2.Domain.Users;
 
-public class Employee
+public class Employee : User
 {
-    
+    public string Department { get; }
+    public string Position { get; }
+
+    public Employee(int id, string firstName, string lastName, string department, string position)
+        : base(id, firstName, lastName, UserType.Employee)
+    {
+        Department = department;
+        Position = position;
+    }
+
+    public override string ToString()
+    {
+        return base.ToString() + $" | Department: {Department} | Position: {Position}";
+    }
 }
--- Zadanie_2/Zadanie_2/Domain/Users/Student.cs
+++ Zadanie_2/Zadanie_2/Domain/Users/Student.cs
@@ -1,7 +1,21 @@
+using Zadanie_2.Domain.Enums;
+
 namespace Zadanie_2.Domain.Users;
 
-public class Student
+public class Student : User
 {
-    
-    
+    public string StudentNumber { get; }
+    public string Faculty { get; }
+
+    public Student(int id, string firstName, string lastName, string studentNumber, string faculty)
+        : base(id, firstName, lastName, UserType.Student)
+    {
+        StudentNumber = studentNumber;
+        Faculty = faculty;
+    }
+
+    public override string ToString()
+    {
+        return base.ToString() + $" | Student No: {StudentNumber} | Faculty: {Faculty}";
+    }
 }
